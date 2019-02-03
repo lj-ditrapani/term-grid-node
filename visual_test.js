73af951dca1bb65b5f99f414d3862ffd826ea25f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-02-03 00:48:04 -0500

    Add some more code to visual test just for fun
    Can do vim like hjkl navigation around a grid.

--- visual_test.js
+++ visual_test.js
@@ -11,16 +11,25 @@ tg.text(3, 2, 'world', mediumCyan, 3)
 tg.draw()
 
 let x = 0
+let y = 0
 process.stdin.on( 'data', function( data ){
   // console.log(`GOT [${data}] length: ${data.length} <${[...data].map(c => c.charCodeAt(0))}>`)
   tg.set(5, 5, 'Y', darkCyan, mediumCyan)
-  if ( data === '\u0061' ) {
-    x += 1
-    tg.set(0, x, '(', cyan, mediumCyan)
+  if ( data === '\u006a' ) {
+    y += 1
+    tg.set(y, x, 'V', cyan, mediumCyan)
   }
-  if ( data === '\u0062' ) {
+  if ( data === '\u006b' ) {
+    y -= 1
+    tg.set(y, x, '^', mediumCyan, cyan)
+  }
+  if ( data === '\u006c' ) {
+    x += 1
+    tg.set(y, x, '(', cyan, mediumCyan)
+  }
+  if ( data === '\u0068' ) {
     x -= 1
-    tg.set(0, x, ')', mediumCyan, cyan)
+    tg.set(y, x, ')', mediumCyan, cyan)
   }
   tg.draw()
   if ( data === '\u0071' ) {
