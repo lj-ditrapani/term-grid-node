a75189214b0e4aad06fe78770f2e1cf5b547002c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-02-09 13:23:55 -0500

    wip test setup

--- test/index_test.ts
+++ test/index_test.ts
@@ -1,9 +1,18 @@
 import { strict as assert } from 'assert'
+import * as sinon from 'sinon'
+import { ReadStream } from 'tty'
+import { Printer, TermGrid } from '../src/index'
 
+// tslint:disable:no-object-literal-type-assertion
 describe('TermGrid', () => {
   describe('clear', () => {
-    it('fails', () => {
+    it('clears the screen', () => {
+      const print = sinon.stub()
+      const printer = { print } as Printer
+      const readStream = (sinon.stub() as unknown) as ReadStream
+      new TermGrid(3, 4, readStream, printer).clear()
       assert(false)
     })
   })
 })
+// tslint:enable:no-object-literal-type-assertion
