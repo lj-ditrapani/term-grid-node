d457e4c5e409872e5004d0c0c168d26ea5b9aa11
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2021-04-22 16:49:47 -0400

    Update examples to use eslint instead of tslint

--- example/usage/usage.ts
+++ example/usage/usage.ts
@@ -3,6 +3,8 @@ import { colors, keyCodes, makeTermGrid } from 'term-grid-ui'
 const tg = makeTermGrid(10, 40)
 tg.clear()
 tg.set(2, 5, ':', colors.black, colors.white)
+tg.text(7, 2, 'Press Enter to see message', colors.black, colors.white)
+tg.text(8, 2, 'Press q to quit', colors.black, colors.white)
 tg.draw()
 tg.onInput((data) => {
   switch (data) {
